595ad1eee238c85ee38ae0ac851a2a9e4800c2ae
Igor <erwaht@gmail.com>
2015-06-03 09:19:41 +0300

    option to remove commiters from the log

--- report.php
+++ report.php
@@ -11,34 +11,61 @@ $repos = array(
 
 $aggregateByDate = 'F Y';
 
+// exclude specific committers from the log
+$excludeCommiters = array('sample@email.com');
+
+$aggregateByDate = 'F Y';
+
 
 $g = new GitReporter();
 
-echo $g->summaryReport($repos, $aggregateByDate, true );
-// echo $g->detailedReport( $repos['buyborg'] );
+echo $g->summaryReport($repos, $aggregateByDate, true, $excludeCommiters );
+
+// echo $g->detailedReport($repos['repo1'], true, $excludeCommiters);
 
 
-Class GitReporter {
+Class GitReporter
+{
 
-    function detailedReport( $repo){
+    function detailedReport($repo, $excludeZero = true, $excludeCommiters = array())
+    {
 
         $r = '';
 
-        if(file_exists( $repo )){
+        if (file_exists($repo)) {
             $log = $this->gitReadCommits($repo);
-            foreach($log as $commit){
+            foreach ($log as $commit) {
                 $data = $this->parseGitMessage($commit['message']);
-		$msg = trim( $data['message'] );
-		
-		$cleanMsg = preg_replace( "/^\[.\]\s*/", '', $msg );
+                $msg = trim($data['message']);
 
-		$timestamp = strtotime($commit['date']);
+                $cleanMsg = preg_replace("/^\[.\]\s*/", '', $msg);
 
-		$timeHours = number_format( (float)$data['time']/3600,2, ",", "" );
+                $timestamp = strtotime($commit['date']);
 
-		
+                $timeHours = number_format((float)$data['time'] / 3600, 2, ",", "");
 
-                $r .= $this->csvRowEncode( array( $timestamp, date('d-m-Y', $timestamp) ,  $cleanMsg, $data['time'], $timeHours ) );
+
+                $author = trim($commit['author']);
+                $result = $this->parseEmail($author);
+                if ($result) {
+                    $author = $result;
+                }
+
+
+                if( !in_array( $author, $excludeCommiters ) && !in_array( $commit['author'], $excludeCommiters ) ) {
+
+                    if (!$excludeZero || ($data['time'] > 0)) {
+
+                        $r .= $this->csvRowEncode(array(
+                            $timestamp,
+                            date('d-m-Y', $timestamp),
+                            $author,
+                            $cleanMsg,
+                            $data['time'],
+                            $timeHours
+                        ));
+                    }
+                }
             }
 
         }
@@ -47,45 +74,47 @@ Class GitReporter {
     }
 
 
-    function summaryReport($repositories, $aggregateByDate, $authorEmailOnly = false) {
-        if( $repositories ){
+    function summaryReport($repositories, $aggregateByDate, $authorEmailOnly = false, $excludeAuthors = array() )
+    {
+        if ($repositories) {
 
             $summary = array();
 
-            foreach( $repositories as $repoName => $repo ){
-                if(file_exists( $repo )){
+            foreach ($repositories as $repoName => $repo) {
+                if (file_exists($repo)) {
                     $log = $this->gitReadCommits($repo);
-                    $resultGroup = $this->processGitLog( $log, $aggregateByDate, $authorEmailOnly );
+                    $resultGroup = $this->processGitLog($log, $aggregateByDate, $authorEmailOnly);
 
 
-
-                    if($resultGroup){
+                    if ($resultGroup) {
                         // print("Repository: $repoName\n");
 
 
-
-                        foreach($resultGroup as $dateLabel => $result){
+                        foreach ($resultGroup as $dateLabel => $result) {
                             // print("\"$dateLabel\"\n");
-                            foreach($result as $author => $worktime){
+                            foreach ($result as $author => $worktime) {
 
-                                $summary[ $dateLabel ][ $author][$repoName] = $worktime;
+                                if( !in_array( $author , $excludeAuthors ) ) {
+
+                                    $summary[$dateLabel][$author][$repoName] = $worktime;
 
 
-                                $workhours = round( $worktime / 3600, 2 );
+                                    // $workhours = round($worktime / 3600, 2);
+                                }
 
                                 // print("\"$author\",$workhours\n");
                             }
                         }
                     } else {
-        //                printf("Input Git Dir was empty: %s\n", $repo);
+                        //                printf("Input Git Dir was empty: %s\n", $repo);
                     }
                 } else {
-        //            printf("Input Git Dir was not found : %s\n", $repo );
+                    //            printf("Input Git Dir was not found : %s\n", $repo );
                 }
             }
 
             // summary CSV
-            return $this->exportCsv( array_keys($repositories), $summary);
+            return $this->exportCsv(array_keys($repositories), $summary);
 
         } else {
             // print("Empty list of repositories");
@@ -95,33 +124,34 @@ Class GitReporter {
 
     //print_r($summary);
 
-    function exportCsv($projects, $summary){
+    function exportCsv($projects, $summary)
+    {
 
         $r = "";
 
-        $row = array( 'date', 'user');
-        foreach( $projects as $project )$row[] = $project;
+        $row = array('date', 'user');
+        foreach ($projects as $project) $row[] = $project;
         $row[] = 'TOTAL';
-        $r.= $this->csvRowEncode($row);
+        $r .= $this->csvRowEncode($row);
 
-        foreach($summary as $dateLabel => $userData){
-            $r.= $this->csvRowEncode( array($dateLabel) );
+        foreach ($summary as $dateLabel => $userData) {
+            $r .= $this->csvRowEncode(array($dateLabel));
 
-            foreach($userData as $user => $projectData ){
+            foreach ($userData as $user => $projectData) {
                 $row = array('', $user);
 
                 $sum = 0;
-                foreach($projects as $project){
-                    if( !empty( $projectData[$project] ) ){
+                foreach ($projects as $project) {
+                    if (!empty($projectData[$project])) {
                         $sum += $projectData[$project];
-                        $row[] = round( $projectData[$project]/3600, 2 );
+                        $row[] = round($projectData[$project] / 3600, 2);
                     } else {
                         $row[] = 0;
                     }
                 }
-                $row[] = round($sum/3600, 2);
+                $row[] = round($sum / 3600, 2);
 
-                $r.= $this->csvRowEncode($row);
+                $r .= $this->csvRowEncode($row);
             }
         }
 
@@ -129,114 +159,113 @@ Class GitReporter {
 
     }
 
-    function csvRowEncode( $row ){
+    function csvRowEncode($row)
+    {
         $rowEnc = array();
-        foreach($row as $r)$rowEnc[] = '"'. str_replace( '"', '""', $r) .'"';
-        return join( ',', $rowEnc)."\n";
+        foreach ($row as $r) $rowEnc[] = '"' . str_replace('"', '""', $r) . '"';
+        return join(',', $rowEnc) . "\n";
     }
 
 
-    function gitReadCommits($dir)    {
-                $output = array();
-                chdir($dir);
-                exec("git log --all",$output);
-                $history = array();
-                //dd($output);
-                $commit = array();
+    function gitReadCommits($dir)
+    {
+        $output = array();
+        chdir($dir);
+        exec("git log --all", $output);
+        $history = array();
+        //dd($output);
+        $commit = array();
 
-                foreach($output as $line){
-
-
-
-                    if(strpos($line, 'commit')===0){
-                        if(!empty($commit)){
-                            array_push($history, $commit);
-                            $commit = array();
-                        }
-                        $commit['hash']   = substr($line, strlen('commit'));
-                    }
-                    else if(strpos($line, 'Author')===0){
-                        $commit['author'] = trim(substr($line, strlen('Author:')), " \t\n\r\0\x0B\"");
-                    }
-                    else if(strpos($line, 'Date')===0){
-                        $commit['date']   = trim(substr($line, strlen('Date:')));
-                    } else {
+        foreach ($output as $line) {
 
 
-                        if(array_key_exists('message', $commit)) {
-                            $commit['message']  .= trim($line).' ';
-                        }else{
-                            $commit['message']  = trim($line);
-                        }
+            if (strpos($line, 'commit') === 0) {
+                if (!empty($commit)) {
+                    array_push($history, $commit);
+                    $commit = array();
+                }
+                $commit['hash'] = substr($line, strlen('commit'));
+            } else if (strpos($line, 'Author') === 0) {
+                $commit['author'] = trim(substr($line, strlen('Author:')), " \t\n\r\0\x0B\"");
+            } else if (strpos($line, 'Date') === 0) {
+                $commit['date'] = trim(substr($line, strlen('Date:')));
+            } else {
 
 
-                    }
+                if (array_key_exists('message', $commit)) {
+                    $commit['message'] .= trim($line) . ' ';
+                } else {
+                    $commit['message'] = trim($line);
                 }
 
-                return $history;
+
+            }
+        }
+
+        return $history;
     }
 
 
-    function parseGitMessage($message){
+    function parseGitMessage($message)
+    {
 
         $defaultMultiplier = 'h';
         $multipliers = array(
-            'd' => 24*3600,
+            'd' => 24 * 3600,
             'h' => 3600,
             'm' => 60,
             's' => 1
         );
 
 
-        if( preg_match("/^\s*\[[\+\*\-]\]\s*(\d+\.?\d*)\s*([dhms])?\s+\-?\s*(.*)$/si", $message, $matches)){
+        if (preg_match("/^\s*\[[\+\*\-]\]\s*(\d+\.?\d*)\s*([dhms])?\s+\-?\s*(.*)$/si", $message, $matches)) {
 
-	    $mult = ( empty($matches[2]) || empty( $multipliers[$matches[2]] ) ) ? $multipliers[$defaultMultiplier] : $multipliers[$matches[2]];
-	
-	
+            $mult = (empty($matches[2]) || empty($multipliers[$matches[2]])) ? $multipliers[$defaultMultiplier] : $multipliers[$matches[2]];
+
+
             $time = (float)$matches[1] * $mult;
 
-            $cleanMessage = trim( $matches[ count($matches) - 1 ] );
+            $cleanMessage = trim($matches[count($matches) - 1]);
 
-	//    if( $time <= 0 ) var_dump($message, $matches, $mult);
+            //    if( $time <= 0 ) var_dump($message, $matches, $mult);
 
 
-            return array('time' => $time, 'message' => $cleanMessage );
+            return array('time' => $time, 'message' => $cleanMessage);
 
         } else {
-            return array( 'time' => 0, 'message' => $message);
+            return array('time' => 0, 'message' => $message);
         }
-
-
 
 
     }
 
-    function processGitLog($input, $dateGroup, $authorEmailOnly ){
+    function processGitLog($input, $dateGroup, $authorEmailOnly)
+    {
 
         $sumByAuthor = array();
 
-        foreach($input as $commit){
+        foreach ($input as $commit) {
 
-                $author = trim( $commit['author'] );
-                if( $authorEmailOnly ){
+            $author = trim($commit['author']);
+            if ($authorEmailOnly) {
 
-                	$result = $this->parseEmail( $author );
-                	if($result){
-                		$author = $result;
-                	}
-                } 
-                $date = date($dateGroup, strtotime($commit['date']));
-
-                $message = $commit['message'];
-
-                $messageData = $this->parseGitMessage($message);
-
-                $time = $messageData['time'];
-
-                if(!isset($sumByAuthor[$date])){
-                    $sumByAuthor[$date] = array();
+                $result = $this->parseEmail($author);
+                if ($result) {
+                    $author = $result;
                 }
-                $sumByAuthor[$date][ $author] = isset( $sumByAuthor[$date][ $author] ) ? $time + $sumByAuthor[$date][ $author] : $time;
+            }
+            $date = date($dateGroup, strtotime($commit['date']));
+
+            $message = $commit['message'];
+
+            $messageData = $this->parseGitMessage($message);
+
+            $time = $messageData['time'];
+
+            if (!isset($sumByAuthor[$date])) {
+                $sumByAuthor[$date] = array();
+            }
+            $sumByAuthor[$date][$author] = isset($sumByAuthor[$date][$author]) ? $time + $sumByAuthor[$date][$author] : $time;
 
         }
 
@@ -246,14 +275,15 @@ Class GitReporter {
 
     }
 
-    function parseEmail($str){
+    function parseEmail($str)
+    {
 
-		preg_match("/<([^>]+)>/",$str,$matches);
-		if( $matches ){
-			return strtolower( $matches[1] );
-		} else {
-			return false;
-		}
+        preg_match("/<([^>]+)>/", $str, $matches);
+        if ($matches) {
+            return strtolower($matches[1]);
+        } else {
+            return false;
+        }
 
     }
 
